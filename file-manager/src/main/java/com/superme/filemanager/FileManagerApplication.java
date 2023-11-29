c75deda6b92b7ec0a59df22417baa0f2ffb182a6
yanruizhi <yanruizhi1018@163.com>
2023-11-29 11:24:22 +0800

    日志

--- file-manager/src/main/java/com/superme/filemanager/FileManagerApplication.java
+++ file-manager/src/main/java/com/superme/filemanager/FileManagerApplication.java
@@ -3,7 +3,7 @@ package com.superme.filemanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.superme.filemanager","com.superme.common"})
+@SpringBootApplication(scanBasePackages = {"com.superme.filemanager"})
 public class FileManagerApplication {
 
     public static void main(String[] args) {
--- login-service/src/main/java/com/superme/loginservice/LoginServiceApplication.java
+++ login-service/src/main/java/com/superme/loginservice/LoginServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.jmx.support.RegistrationPolicy;
 
-@SpringBootApplication(scanBasePackages = {"com.superme.common","com.superme.loginservice"})
+@SpringBootApplication(scanBasePackages = {"com.superme.loginservice"})
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)//组止注册jedisPool时报错"MBean已存在"
 public class LoginServiceApplication {
 
